10daec617b3ac00b4b91a1e4dd5194b0ee0d8942
Mrigesh Kumar Sharma <37233391+Mrigesh16@users.noreply.github.com>
2026-06-20 21:23:32 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String encode(List<String> strs) {
+        if (strs.size() == 0)
+            return Character.toString((char) 258);
+        String seperater = Character.toString((char) 257);
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str);
+            sb.append(seperater);
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        if (str.equals(Character.toString((char) 258)))
+            return new ArrayList<>();
+        String seperater = Character.toString((char) 257);
+        return Arrays.asList(str.split(seperater, -1));
+    }
+}
